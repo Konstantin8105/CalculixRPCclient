da4f218e055277be053c397412a20230533662de
konstantin8105 <konstantin8105@gmail.com>
2017-04-16 22:13:03 +0300

    modify Dat client

---
+++ clientCalculix/calcBuckle.go
@@ -0,0 +1,79 @@
+package clientCalculix
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+// CalculateForBuckle - calculation
+func (c *ClientCalculix) CalculateForBuckle(inpBody []string) (factors []float64, err error) {
+	dats, err := c.CalculateForDat(inpBody)
+	for _, dat := range dats {
+		factor, err := getBucklingFactor(dat)
+		if err != nil {
+			return factors, err
+		}
+		factors = append(factors, factor)
+	}
+	return factors, nil
+}
+
+func getBucklingFactor(dat string) (factor float64, err error) {
+	lines := strings.Split(dat, "\n")
+
+	bucklingHeader := "B U C K L I N G   F A C T O R   O U T P U T"
+	var found bool
+	var numberLine int
+	amountBuckling := 1
+	for _, line := range lines {
+		line = strings.TrimSpace(line)
+		if !found {
+			// empty line
+			if len(line) == 0 {
+				continue
+			}
+			if len(line) != len(bucklingHeader) {
+				continue
+			}
+			if line == bucklingHeader {
+				found = true
+			}
+		} else {
+			numberLine++
+			if numberLine >= 5+amountBuckling {
+				break
+			}
+			if numberLine >= 5 {
+				m, f, err := parseBucklingFactor(line)
+				if err != nil {
+					return -1., err
+				}
+				if m != numberLine-4 {
+					return -1., fmt.Errorf("Wrong MODE NO: %v (%v) in line: %v", m, numberLine-4, line)
+				}
+				return f, nil
+			}
+		}
+	}
+	return -1., fmt.Errorf("Cannot found in dat")
+}
+
+// Example:
+//      4   0.4067088E+03
+func parseBucklingFactor(line string) (mode int, factor float64, err error) {
+	s := strings.Split(line, "   ")
+	for i := range s {
+		s[i] = strings.TrimSpace(s[i])
+	}
+	i, err := strconv.ParseInt(s[0], 10, 64)
+	if err != nil {
+		return 0, 0, fmt.Errorf("Error: string parts - %v, error - %v, in line - %v", s, err, line)
+	}
+	mode = int(i)
+	factor, err = strconv.ParseFloat(s[1], 64)
+	if err != nil {
+		return 0, 0, fmt.Errorf("Error: string parts - %v, error - %v, in line - %v", s, err, line)
+	}
+	return mode, factor, nil
+}
--- clientCalculix/calcDat.go
+++ clientCalculix/calcDat.go
@@ -2,34 +2,87 @@ package clientCalculix
 
 import (
 	"fmt"
+	"sync"
 
 	"github.com/Konstantin8105/CalculixRPCserver/serverCalculix"
 )
 
 // CalculateForDat - calculation
 func (c *ClientCalculix) CalculateForDat(inpBody []string) (datBody []string, err error) {
-	for _, inp := range inpBody {
-	BACK:
-		client, err := c.getServer()
-		defer func() {
-			err2 := client.Close()
-			if err2 != nil {
-				err = fmt.Errorf("Errors:%v\n%v", err2, err)
-			}
-		}()
-		if err.Error() == serverCalculix.ErrorServerBusy {
-			goto BACK
-		}
-		if err != nil {
-			return datBody, err
-		}
 
-		var dat serverCalculix.DatBody
-		err = client.Call("Calculix.ExecuteForDat", inp, &dat)
-		if err != nil {
-			return datBody, err
-		}
-		datBody = append(datBody, dat.A)
+	type block struct {
+		inp string
+		dat string
 	}
+	var blockData []block
+	blockChannel := make(chan block)
+	errChannel := make(chan error)
+
+	var wg sync.WaitGroup
+
+	go func() {
+		for block := range blockChannel {
+			blockData = append(blockData, block)
+		}
+	}()
+
+	go func() {
+		for e := range errChannel {
+			err = fmt.Errorf("Error: %v\n%v", e, err)
+		}
+	}()
+
+	for _, inp := range inpBody {
+		// Increment the WaitGroup counter.
+		wg.Add(1)
+
+		// Launch a goroutine to fetch the inp file.
+		go func(inp string) {
+			// Decrement the counter when the goroutine completes.
+			defer wg.Done()
+		BACK:
+			client, err := c.getServer()
+			defer func() {
+				err2 := client.Close()
+				if err2 != nil {
+					errChannel <- fmt.Errorf("Errors:%v\n%v", err2, err)
+				}
+			}()
+			if err != nil {
+				if err.Error() == serverCalculix.ErrorServerBusy {
+					goto BACK
+				}
+				errChannel <- err
+			}
+
+			var dat serverCalculix.DatBody
+			err = client.Call("Calculix.ExecuteForDat", inp, &dat)
+			if err != nil {
+				if err.Error() == serverCalculix.ErrorServerBusy {
+					goto BACK
+				}
+				errChannel <- err
+			}
+			blockChannel <- block{inp: inp, dat: dat.A}
+		}(inp)
+	}
+	// Wait for all inp body
+	wg.Wait()
+
+	// Close all opened channels
+	close(errChannel)
+	close(blockChannel)
+
+	//repair sequene of result dat
+	for _, inp := range inpBody {
+		for _, block := range blockData {
+			if inp == block.inp {
+				datBody = append(datBody, block.dat)
+				goto NewInp
+			}
+		}
+	NewInp:
+	}
+
 	return datBody, nil
 }
